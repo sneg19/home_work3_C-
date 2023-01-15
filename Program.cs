0d5226ddef2b1b5a6894fb97b401e644890ecefc
sneg <sneg.sbg@gmail.com>
2023-01-15 16:31:54 +0300

    %==0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите любое число: ");
+int number = Int32.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} четное");
+}
+else 
+{
+    Console.WriteLine($"{number} нечетное");
+}
